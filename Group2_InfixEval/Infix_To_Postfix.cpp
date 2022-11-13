b6dc4d8153410ba7afc7845801dee8f0f69871d4
RobertBlocker <99045922+RobertBlocker@users.noreply.github.com>
2022-11-13 14:06:59 -0600

    Update Infix_To_Postfix.cpp

--- Group2_InfixEval/Infix_To_Postfix.cpp
+++ Group2_InfixEval/Infix_To_Postfix.cpp
@@ -4,11 +4,20 @@
 
 using namespace std;
 
+/** Returns if character is an operator.
+    @param op: operator for operation
+    @return: {true} if character is operator; {false} otherwise
+*/
 bool is_operator(char op)
 {
     return (op == '|' || op == '&' || op == '=' || op == '!' || op == '>' || op == '<' || op == '+' || op == '-' || op == '*' || op == '/' || op == '%' || op == '^');
 }
 
+/** Returns the precedence of an operator.
+    @param op: operator to return its precedence
+    @return: precedence
+    @throws exception: operator unsupported
+*/
 int precedence(char op) {
 
     if (op == '|') {
@@ -32,42 +41,61 @@ int precedence(char op) {
     else if (op == '^') {
         return 7;
     }
-
+    throw exception("Unsupported operator");
     return 0;
 }
 
-string infix_to_postfix(string infix)
+/** Converts an infix expression to postfix expression.
+    @param infix_exp: infix expression to convert
+    @return: postfix expression converted from the infix expression
+*/
+string infix_to_postfix(string& infix_exp)
 {
     int i = 0;
     stack<char> stack;
     string postfix;
 
-    for (char ch : infix)
-    {
-        if (!is_operator(ch))
-        {
-            postfix = postfix + infix[i];
+    for (char ch : infix_exp) {
+        //if open parenthesis is found
+        if (ch == '(') { stack.push(ch); }
+        //if closing parenthesis
+        else if (ch == ')') {
+            // Add each operator to the postfix expression string.
+            // Pop all operators from the stack, until an opening parenthesis is seen on top of the stack.
+            while (stack.top() != '(') {
+                postfix += stack.top();
+                stack.pop();
+            }
+            //pop opening '('
+            stack.pop();
+        }
+
+        //find numbers
+        if (!is_operator(ch)) {
+            postfix = postfix + ch;
             i++;
         }
-        else
-        {
-            while (!stack.empty() && precedence(infix[i]) <= precedence(stack.top()))
+
+        //else statement to push operators onto stack and add operators to postfix expression
+        else {
+            while (!stack.empty() && precedence(ch) <= precedence(stack.top()))
             {
                 postfix += stack.top();
                 stack.pop();
             }
-            stack.push(infix[i]);
+            stack.push(ch);
             i++;
         }
     }
 
-    while (!stack.empty())
-    {
+    while (!stack.empty()) {
         postfix = postfix + stack.top();
         stack.pop();
     }
+    //Take all whitespace from string and remove
     postfix.erase(remove_if(postfix.begin(), postfix.end(), ::isspace), postfix.end());
     string postfix_to_eval;
+    //New string that adds space between all char and return string
     for (auto itr : postfix) {
         postfix_to_eval += itr;
         postfix_to_eval += ' ';
@@ -76,10 +104,10 @@ string infix_to_postfix(string infix)
 }
 
 int main() {
-    string infix = "2+2^2*3";
-//   cout << "infix expression: ";
-//    getline(cin, infix);
-    string postfix = infix_to_postfix(infix);
+    string infix_exp = "2+2^2*3+(5-3)";
+//    cout << "infix expression: ";
+//    getline(cin, infix_exp);
+    string postfix = infix_to_postfix(infix_exp);
     cout << "converted to postfix: " << postfix << endl;
 
     system("pause");
