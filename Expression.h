066682d6d59cc24a69ee1329cf60f6a282462b08
bryce0110 <bdt0110@yahoo.com>
2022-11-12 21:44:29 -0600

    Initial Commit

---
+++ Expression.h
@@ -0,0 +1,20 @@
+﻿#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+#include <stack>
+#include <string>
+
+class Expression {
+public:
+
+    // Class-member functions
+    int precedence(char);
+    int eval(std::string);
+
+private:
+
+    std::stack<int> vals;
+    std::stack<std::string> ops;
+};
+
+#endif
