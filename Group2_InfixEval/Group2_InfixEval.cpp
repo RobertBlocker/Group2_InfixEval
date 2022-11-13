a0110cb87318805e90fb8a8f6a1a5f1303fc5051
bryce0110 <bdt0110@yahoo.com>
2022-11-13 14:34:54 -0600

    Basic Input/Output

---
+++ Group2_InfixEval/Group2_InfixEval.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    if (!fin) {
+        cout << "The input file does not exist." << endl;
+        system("pause");
+        return -1;
+    }
+    
+    string exp;
+    string eval;
+
+    while(getline(fin, exp)) {
+        cout << exp << endl;
+        //eval = evaluate(infix_to_postfix(exp));
+
+        fout << eval << endl;
+    }
+
+    fin.close();
+    fout.close();
+    
+    return 0;
+}
