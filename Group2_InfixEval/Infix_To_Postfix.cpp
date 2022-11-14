31aeec8cc6109ee7b847cd0b1bf2da7271cfc1b3
RobertBlocker <99045922+RobertBlocker@users.noreply.github.com>
2022-11-13 19:08:20 -0600

    Updated for operator fixes

--- Group2_InfixEval/Infix_To_Postfix.cpp
+++ Group2_InfixEval/Infix_To_Postfix.cpp
@@ -1,7 +1,6 @@
-#include "Infix_To_Postfix.h"
-
 #include <iostream>
 #include <stack>
+#include <string>
 
 using namespace std;
 
@@ -42,6 +41,7 @@ int precedence(char op) {
     else if (op == '^') {
         return 7;
     }
+    
     return 0;
 }
 
@@ -49,47 +49,87 @@ int precedence(char op) {
     @param infix_exp: infix expression to convert
     @return: postfix expression converted from the infix expression
 */
-string infix_to_postfix(string& infix_exp)
-{
-    int i = 0;
+string infix_to_postfix(string& infix_exp) {
+    //int i = 0;
     stack<char> stack;
     string postfix;
 
-    for (char ch : infix_exp) {
+    for (int i = 0; i < infix_exp.size(); i++) {
         //if open parenthesis is found
-        if (ch == '(') { stack.push(ch); }
+        if (infix_exp[i] == '(') { stack.push(infix_exp[i]); }
         //if closing parenthesis
-        else if (ch == ')') {
+        else if (infix_exp[i] == ')') {
             // Add each operator to the postfix expression string.
             // Pop all operators from the stack, until an opening parenthesis is seen on top of the stack.
             while (stack.top() != '(') {
                 postfix += stack.top();
                 stack.pop();
+                i++;
             }
             //pop opening '('
             stack.pop();
         }
 
         //find numbers
-        if (!is_operator(ch)) {
-            postfix = postfix + ch;
-            i++;
+        if (!is_operator(infix_exp[i])) {
+            postfix += infix_exp[i];
+
         }
 
         //else statement to push operators onto stack and add operators to postfix expression
         else {
-            while (!stack.empty() && precedence(ch) <= precedence(stack.top()))
-            {
+            while (!stack.empty() && stack.top() != '(' && precedence(infix_exp[i]) <= precedence(stack.top())) {
                 postfix += stack.top();
                 stack.pop();
             }
-            stack.push(ch);
-            i++;
+
+            /* If the current operator is two characters long we:
+               1) skip the char after the first one is recognized (| is guaranteed to be follow by another |, so use | to represent || skipping the second |) 
+               2) or by using a different symbol to recognize it (< can be < or <=, so we use # to represent it and $ for >=)
+               We increment i to skip the next character, if the next character creates a two character long operator
+            */
+            if (infix_exp[i] == '>') {
+                i++;
+                if (infix_exp[i] == '=') { stack.push('$');  }
+                else {
+                    stack.push('>');
+                    i--;
+                }
+                
+            }
+            else if (infix_exp[i] == '<') {
+                i++;
+                if (infix_exp[i] == '=') { stack.push('#');  }
+                else {
+                    stack.push('<');
+                    i--;
+                }
+            }
+            else if (infix_exp[i] == '=') {                
+                stack.push('=');
+                i++;
+            }
+            else if (infix_exp[i] == '!') {
+                stack.push('!');
+                i++;
+            }
+            else if (infix_exp[i] == '&') {               
+                stack.push('&');
+                i++;
+            }
+            else if (infix_exp[i] == '|') {
+                stack.push('|');
+                i++;
+            }
+            else {
+                stack.push(infix_exp[i]);
+
+            }
         }
     }
 
     while (!stack.empty()) {
-        postfix = postfix + stack.top();
+        postfix += stack.top();
         stack.pop();
     }
     //Take all whitespace from string and remove
@@ -98,11 +138,21 @@ string infix_to_postfix(string& infix_exp)
     postfix.erase(remove(postfix.begin(), postfix.end(), '('), postfix.end());
     postfix.erase(remove(postfix.begin(), postfix.end(), ')'), postfix.end());
     string postfix_to_eval;
-    
     //New string that adds space between all char and return string
     for (auto itr : postfix) {
         postfix_to_eval += itr;
         postfix_to_eval += ' ';
     }
     return postfix_to_eval;
+}
+
+int main() {
+    string infix_exp = "88 + 5 * 4 ";
+//    cout << "infix expression: ";
+//    getline(cin, infix_exp);
+    string postfix = infix_to_postfix(infix_exp);
+    cout << "converted to postfix: " << postfix << endl;
+
+    system("pause");
+    return 0;
 }
